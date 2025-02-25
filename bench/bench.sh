f13648d7a8ce3e1f41e73ad2759a28b4ce2cb327
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2025-02-25 14:53:07 +0100

    Added other benchmark datasets.

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,31 +1,38 @@
-# python bench.py -c 1089_USCrime.yaml 1089_USCrime
+python bench.py -c 1089_USCrime.yaml 1089_USCrime
 python bench.py -c 1089_USCrime.yaml 1027_ESL
+python bench.py -c 1089_USCrime.yaml 1028_SWD
 python bench.py -c 1089_USCrime.yaml 1029_LEV
-# python bench.py -c 1089_USCrime.yaml 1096_FacultySalaries
-# python bench.py -c 1089_USCrime.yaml 192_vineyard
-python bench.py -c 1089_USCrime.yaml 195_auto_price
-# python bench.py -c 1089_USCrime.yaml 210_cloud
-# python bench.py -c 1089_USCrime.yaml 228_elusage
+python bench.py -c 1089_USCrime.yaml 1030_ERA
+python bench.py -c 1089_USCrime.yaml 1096_FacultySalaries
+python bench.py -c 1089_USCrime.yaml 192_vineyard
+python bench.py -c 1089_USCrime.yaml 197_cpu_act
+python bench.py -c 1089_USCrime.yaml 210_cloud
+python bench.py -c 1089_USCrime.yaml 225_puma8NH
+python bench.py -c 1089_USCrime.yaml 227_cpu_small
+python bench.py -c 1089_USCrime.yaml 228_elusage
 python bench.py -c 1089_USCrime.yaml 230_machine_cpu
-# python bench.py -c 1089_USCrime.yaml 523_analcatdata_neavote
+python bench.py -c 1089_USCrime.yaml 523_analcatdata_neavote
 python bench.py -c 1089_USCrime.yaml 547_no2
-# python bench.py -c 1089_USCrime.yaml 663_rabe_266
+python bench.py -c 1089_USCrime.yaml 663_rabe_266
 python bench.py -c 1089_USCrime.yaml 666_rmftsa_ladata
-# python bench.py -c 1089_USCrime.yaml 678_visualizing_environmental
-# python bench.py -c 1089_USCrime.yaml 687_sleuth_ex1605
+python bench.py -c 1089_USCrime.yaml 678_visualizing_environmental
+python bench.py -c 1089_USCrime.yaml 687_sleuth_ex1605
 python bench.py -c 1089_USCrime.yaml 695_chatfield_4
-# python bench.py -c 1089_USCrime.yaml 706_sleuth_case1202
+python bench.py -c 1089_USCrime.yaml 706_sleuth_case1202
 python bench.py -c 1089_USCrime.yaml 712_chscase_geyser1
 python bench.py -c 1089_USCrime.yaml 229_pwLinear
-# python bench.py -c 1089_USCrime.yaml 485_analcatdata_vehicle
+python bench.py -c 1089_USCrime.yaml 485_analcatdata_vehicle
 python bench.py -c 1089_USCrime.yaml 522_pm10
-# python bench.py -c 1089_USCrime.yaml 527_analcatdata_election2000
-# python bench.py -c 1089_USCrime.yaml 542_pollution
+python bench.py -c 1089_USCrime.yaml 527_analcatdata_election2000
+python bench.py -c 1089_USCrime.yaml 542_pollution
 python bench.py -c 1089_USCrime.yaml 556_analcatdata_apnea2
 python bench.py -c 1089_USCrime.yaml 557_analcatdata_apnea1
 python bench.py -c 1089_USCrime.yaml 560_bodyfat
 python bench.py -c 1089_USCrime.yaml 561_cpu
-# python bench.py -c 1089_USCrime.yaml 659_sleuth_ex1714
-# python bench.py -c 1089_USCrime.yaml 665_sleuth_case2002
+python bench.py -c 1089_USCrime.yaml 659_sleuth_ex1714
+python bench.py -c 1089_USCrime.yaml 665_sleuth_case2002
 python bench.py -c 1089_USCrime.yaml 690_visualizing_galaxy
 python bench.py -c 1089_USCrime.yaml 519_vinnie
+python bench.py -c 1089_USCrime.yaml 529_pollen
+python bench.py -c 1089_USCrime.yaml 503_wind
+python bench.py -c 1089_USCrime.yaml 505_tecator
